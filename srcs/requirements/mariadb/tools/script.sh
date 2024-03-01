529caa1850691debfcb23497b1842503b494d2c1
aarchtou <aarchtou@aarchtou.42.fr>
2024-03-01 08:03:59 +0100

    done

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+
+# mysql_install_db
+mysqld_safe &
+
+sleep 5
+
+mariadb -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+EOF
+mariadb -u root <<EOF
+CREATE USER '$mariadb_USER'@'%' IDENTIFIED BY '$mariadb_PASSWORD';
+EOF
+mariadb -u root <<EOF
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO $mariadb_USER@'%';
+EOF
+mariadb -u root <<EOF
+FLUSH PRIVILEGES;
+EOF
+
+mariadb -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$mariadb_ROOT_PASSWORD';
+EOF
+
+mariadb -u root -p${mariadb_ROOT_PASSWORD} <<EOF
+shutdown
+EOF
+
+mysqld
+
+# mysqladmin shutdown -p${mariadb_ROOT_PASSWORD}
+# wp config set SERVER_PORT 3306 --allow-root
+# wp config set DB_NAME aarchtou42 --allow-root --path=.
+# wp config set DB_USER bele --allow-root --path=.
+# wp config set DB_PASSWORD 12 --allow-root --path=.
+# wp config set DB_PASSWORD 1234 --allow-root --path=.
+#wp config set DB_HOST 'mariadb:3306' --allow-root --path=.
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sleep  20
+
+cd /var/www/html
+wp core download  --allow-root
+mv wp-config-sample.php wp-config.php
+
+
+wp config set SERVER_PORT 3306 --allow-root
+wp config set DB_NAME $MYSQL_DATABASE --allow-root  --path=/var/www/html
+wp config set DB_USER $mariadb_USER --allow-root --path=/var/www/html
+wp config set DB_PASSWORD $mariadb_PASSWORD --allow-root --path=/var/www/html
+wp config set DB_HOST 'mariadb' --allow-root --path=/var/www/html
+
+wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD --admin_email=aarchtou@student.1337.ma --allow-root --path=/var/www/html
+
+
+wp user create $MYSQL_USER randomMail@gmail.com --role=author --user_pass=$MYSQL_PASSWORD  --allow-root --path=/var/www/html
+
+sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.4/fpm/pool.d/www.conf
+
+
+/usr/sbin/php-fpm7.4 -F
